917f594eb7bc91cbaf66c7c0bb364689a6314e6b
Leny <shondayD42@gmail.com>
2022-02-03 12:05:14 +0100

    sql

--- src/Controller/ServDuchandController.php
+++ src/Controller/ServDuchandController.php
@@ -7,6 +7,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\Utilisateur;
 
 class ServDuchandController extends AbstractController
 {
